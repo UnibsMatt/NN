d2c7471cbd50db414a8bb4ad1301c334f40a579d
mattia <masdmasd92@gmail.com>
2020-03-01 22:55:48 +0100

    it begin

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from Nets.Networks import *
-
+import torch
+import numpy as np
 
 model = Net()
 
-print net
+inputs = torch.randint(0, 255, (64, 1, 28, 28)).float()
+
+for i in range(1000):
+    ou = model(inputs)
+    print(ou)
